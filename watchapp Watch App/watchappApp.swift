22f427a0ce258a92921eb90f4c251b7ad2a0dee5
Ivan Schuetz <ivanschuetz@gmail.com>
2025-01-22 19:11:46 +0100

    add watch target

---
+++ watchapp Watch App/watchappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchappApp.swift
+//  watchapp Watch App
+//
+//  Created by Ivan Schuetz on 22.01.25.
+//
+
+import SwiftUI
+
+@main
+struct watchapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
