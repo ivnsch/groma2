f8b59a048b60e9eff46a11931143680f6e57a26b
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-06 08:15:47 +0100

    fix on SE only showing first line of empty view text

--- groma_new/EmptyView.swift
+++ groma_new/EmptyView.swift
@@ -4,17 +4,16 @@ struct EmptyView: View {
     let message: String
 
     var body: some View {
-        ZStack {
-            VStack {
-                Image(.peel)
-                   .resizable()
-                   .frame(width: 200, height: 128)
-                   .buttonStyle(PlainButtonStyle())
-                Text(message)
-                    .multilineTextAlignment(.center)
-//                    .padding(.top, 20)
-            }
-            .padding(.top, 100)
+        VStack {
+            Image(.peel)
+               .resizable()
+               .frame(width: 200, height: 128)
+               .buttonStyle(PlainButtonStyle())
+            Text(message)
+                .multilineTextAlignment(.center)
+                .fixedSize(horizontal: false, vertical: true) // Force height expansion
+                .padding(.horizontal, 20)
         }
+        .padding(.top, 100)
     }
 }
